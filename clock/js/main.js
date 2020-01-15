d2e8d4a33720e34efb60d17dc9a660b4b7d6009f
Paul Burkart <paulburkat@protonmail.com>
2020-01-15 11:12:52 -0500

    Formatting & Style

--- clock/js/main.js
+++ clock/js/main.js
@@ -1,12 +1,18 @@
 let div = document.getElementById('timestamp');
 function updateTime() {
 	let d = new Date();
-	let s = d.getSeconds();
-	let m = d.getMinutes();
+	let s = formatTime(d.getSeconds());
+	let m = formatTime(d.getMinutes());
 	let h = d.getHours();
 	div.innerHTML = h + ":" + m + ":" + s;
 }
 
+function formatTime(x) {
+	if(x<10){
+		x = "0" + x;
+	}
+	
+	return x;
+}
+
 setInterval(updateTime, 1000);
-
-
